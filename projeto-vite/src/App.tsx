80c907e6f8a44786fd6cae7e71c1d126475841d8
Flávio Marques <flaviomarques2@hotmail.com>
2023-06-06 11:16:16 -0300

    passando map para importar a lista de pokemons

--- projeto-vite/src/App.tsx
+++ projeto-vite/src/App.tsx
@@ -1,26 +1,32 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
 import SearchBar from './components/SearchBar/SearchBar'
 import axios from 'axios'
 
 function App() {
-  useEffect(()=> {
-    axios.get('https://pokeapi.co/api/v2/pokemon/')
-      //useEffect com o axios.get retorna uma Promise, ou seja, podemos usar async/await ou then/catch
-      .then((res) => {
-        console.log(res)
-      })
-      // .catch((err) => {
-      //   console.log(err)
-      // });
-  })
+  const [pokemons, setPokemons] = useState ([])
 
+  useEffect(()=> {
+    axios
+      .get('https://pokeapi.co/api/v2/pokemon')
+      //useEffect com o axios.get retorna uma Promise, ou seja, podemos usar async/await ou then/catch
+      .then((res) => setPokemons(res.data.results))
+      .catch((err) => {console.log(err)})
+      
+  }, []);
+
+  
+  <ul>
+    {pokemons && pokemons.map((pokemon) => <li key={pokemon}>{pokemon}</li>)}
+  </ul>
+  
+  
   return (
     <>
       <Navbar/>
       <SearchBar/>
     </>
-  )
+  );
 }
 
 export default App
