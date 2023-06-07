6b1f055928e91d3290ff080ff7fe94e68cde730b
Flávio Marques <flaviomarques2@hotmail.com>
2023-06-07 16:58:57 -0300

    Corrigi estilos dos cards dos pokemons

--- projeto-vite/src/App.tsx
+++ projeto-vite/src/App.tsx
@@ -2,33 +2,33 @@ import { useEffect, useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
 import SearchBar from './components/SearchBar/SearchBar'
 import axios from 'axios'
-import '../App.css'
+import './App.css'
 
 function App() {
 
   interface Pokemon {
     name: string;
+    imagem: string;
   }
 
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
 
   useEffect(()=> {
     axios
-      .get('https://pokeapi.co/api/v2/pokemon')
+      .get('https://pokeapi.co/api/v2/pokemon?limit=151')
       //useEffect com o axios.get retorna uma Promise, ou seja, podemos usar async/await ou then/catch
       .then((res) => setPokemons(res.data.results))
       .catch((err) => {console.log(err)})
       
   }, []);
-
-  
+   
   return (
     <>
       <Navbar/>
       <SearchBar/>
       <div className='lista-pokemon'>
         <ul>
-          {pokemons && pokemons.map((pokemon) => <li key={pokemon.name}>{pokemon.name}</li>)}
+          {pokemons && pokemons.map((pokemon) => <li key={pokemon.name}><p>{pokemon.name}</p><img src={pokemon.imagem}/></li>)}
         </ul>
       </div>
     </>
